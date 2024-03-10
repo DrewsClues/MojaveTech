76a7e8b4319bf99d7b308b82242c86121122dac7
DrewsClues <at869@nau.edu>
2024-03-10 17:47:20 +0900

    help-faq added

--- src/components/help-section.jsx
+++ src/components/help-section.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from 'react';
-import { FaDotCircle } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { RiTwitterXFill } from "react-icons/ri";
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
+import Accordion from 'react-bootstrap/Accordion';
 
 import '../css-sheets/help-section.css';
+import { AiOutlineTool } from "react-icons/ai";
+import { LuBadgeHelp } from "react-icons/lu";
+import { FaHandsHelping } from "react-icons/fa";
+
+
+
+
 
 function Help_section() {
 
@@ -19,9 +21,29 @@ function Help_section() {
 
                 <div className='help-container-body'> 
 
-                  <div className='help-text1-div'>
+                  <div className='help-card'>
+                    <FaHandsHelping className='help-icon'/>
+                    <h3>General Help</h3>
+                    <p className='help-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                      eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                  </div>
 
+                  <div className='help-card'>
+                    <LuBadgeHelp className='help-icon'/>
+                    <h3>Common Questions</h3>
+                    <p className='help-paragraph'>Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat.</p>
+                  </div>
 
+                  <div className='help-card'>
+                    <AiOutlineTool className='help-icon'/>
+                    <h3>Client Support</h3>
+                    <p className='help-paragraph'>Duis aute irure dolor in
+                      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                      culpa qui officia deserunt mollit anim id est laborum.
+                      </p>
                   </div>
 
 
